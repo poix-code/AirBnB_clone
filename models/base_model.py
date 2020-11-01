eed8133a2c878752b20ee3eb0779df291f10a2cc
poix-code <1996@holbertonschool.com>
2020-11-01 18:15:46 +0000

    Added the base class 'BaseModel' with its respectly instances and methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
-""""""
+"""The base class and import modules are defined"""
+import uuid
+from datetime import datetime
+import json
+
+
+class BaseModel():
+    """The instances and methods of the class are defined"""
+    def __init__(self):
+        """Public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Print the instances with the magic method __str__"""
+        return "[{}] ({}) {}".format(BaseModel.__name__,
+                                     self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute 'updated_ at',
+        with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary with the key/values of __dict__"""
+        self.__dict__['__class__'] = 'BaseModel'
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        return self.__dict__
