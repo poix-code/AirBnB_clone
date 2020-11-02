f9811107a11c1b2727ea12af0e66e222647d1155
poix-code <1996@holbertonschool.com>
2020-11-02 22:59:05 +0000

    The process of serialization-deserialization was modified in the way of '<class 'BaseModel'> -> to_dict() -> <class 'dict'> -> JSON dump -> <class 'str'> -> FILE -> <class 'str'> -> JSON load -> <class 'dict'> -> <class 'BaseModel'>'

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,18 @@
 import uuid
 from datetime import datetime
 import json
+import models
 
 
 class BaseModel():
     """The instances and methods of the class are defined"""
-    def __init__(self, name=None, my_number=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Public instance attributes"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
         else:
             for i in kwargs:
                 if i == '__class__':
@@ -33,10 +35,12 @@ class BaseModel():
         """Updates the public instance attribute 'updated_ at',
         with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary with the key/values of __dict__"""
-        self.__dict__['__class__'] = 'BaseModel'
-        self.__dict__['created_at'] = self.created_at.isoformat()
-        self.__dict__['updated_at'] = self.updated_at.isoformat()
-        return self.__dict__
+        cpy = self.__dict__.copy()
+        cpy['__class__'] = 'BaseModel'
+        cpy['created_at'] = self.created_at.isoformat()
+        cpy['updated_at'] = self.updated_at.isoformat()
+        return cpy
