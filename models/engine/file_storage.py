c500d38f53582ab3f755026899d449d1a5d2c500
hfog2066 <hfog2000@yahoo.com>
2020-11-04 00:52:42 -0500

    file_storage.py modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,22 +26,22 @@ class FileStorage:
     def new(self, obj):
         """Sets the obj in a key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__+'.'+obj.id)
-        self.__objects[key] = obj
+        type(self).__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         new_dict = {}
         for key, value in self.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(self.__file_path, "w", encoding="utf-8") as write_file:
+        with open(type(self.__file_path, "w", encoding="utf-8")) as write_file:
             write_file.write(json.dump(new_dict))
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as read_file:
+            with open(type(self.__file_path, "r", encoding="utf-8")) as read_file:
                 for key, value in temp.items():
                     temp_value = models.clases[value["__class__"]](**value)
-                    self.__objects[key] = temp_value
+                    type(self).__objects[key] = temp_value
         except FileNotFoundError:
             pass
