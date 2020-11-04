58e945e4f7273b1223dff8d83a5399bb01265a22
hfog2066 <hfog2000@yahoo.com>
2020-11-03 20:27:57 -0500

    engine/file_storage.py modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """A class FileStorage that serializes instances to a JSON file and
  deserializes JSON file to instances"""
-from models.base_model import BaseModel
+
 import json
+import os
+
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
