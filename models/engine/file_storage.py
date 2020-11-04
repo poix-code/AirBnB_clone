592fcbac0a2cb714c3cd451dfeea42c56dd1b02b
hfog2066 <hfog2000@yahoo.com>
2020-11-03 23:59:13 -0500

    file_storage.py modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,50 +2,47 @@
 """A class FileStorage that serializes instances to a JSON file and
  deserializes JSON file to instances"""
 
-from models.base_model import BaseModel
-from models.user import User
 import json
-from datetime import datetime
-from models.place import Place
-from models.city import City
-from models.state import State
-from models.review import Review
+import os
+
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
     """Represents a class FileStorage"""
-
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """Returns a dictionary"""
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
-        """sets in objects"""
+        """Sets the obj in a key <obj class name>.id"""
         key = type(obj).__name__ + '.' + obj.id
-        type(self).__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         new_dict = {}
-        for key, value in FileStorage.__objects.items():
+        for key, value in type(self).__objects.items():
             new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w",
-                  encoding='utf-8') as write_file:
-            json.dump(new_dict, write_file)
+        with open(type(self).__file_path, "w") as fd:
+            json.dump(new_dict, fd)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, "r",
-                      encoding='utf-8') as read_file:
-                type(self).__objects = json.load(read_file)
-            for key, value in type(self).__objects.items():
-                obj = eval(type(self).__objects[key]['__class__'])(**value)
-                type(self).__objects[key] = value
+            with open(type(self).__file_path, "r") as read_file:
+                for key, value in (json.load(read_file)).items():
+                    value = eval((value["__class__"]) + "(**value)")
+                    type(self).__objects[key] = value
 
         except FileNotFoundError:
             pass
