81cf1003324d524a2829cabfcddcf50708d7b56c
hfog2066 <hfog2000@yahoo.com>
2020-11-04 00:55:58 -0500

    file_storage.py modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,8 +3,7 @@
  deserializes JSON file to instances"""
 
 import json
-import os
-
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -25,23 +24,23 @@ class FileStorage:
 
     def new(self, obj):
         """Sets the obj in a key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__+'.'+obj.id)
-        type(self).__objects[key] = obj
+        key = type(obj).__name__ + '.' + obj.id
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
         new_dict = {}
-        for key, value in self.__objects.items():
+        for key, value in type(self).__objects.items():
             new_dict[key] = value.to_dict()
-        with open(type(self.__file_path, "w", encoding="utf-8")) as write_file:
-            write_file.write(json.dump(new_dict))
+        with open(type(self).__file_path, "w") as fd:
+            json.dump(new_dict, fd)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(type(self.__file_path, "r", encoding="utf-8")) as read_file:
-                for key, value in temp.items():
-                    temp_value = models.clases[value["__class__"]](**value)
-                    type(self).__objects[key] = temp_value
+            with open(type(self).__file_path, "r") as read_file:
+                for key, value in (json.load(read_file)).items():
+                    value = eval((value["__class__"]) + "(**value)")
+                    type(self).__objects[key] = value
         except FileNotFoundError:
             pass
