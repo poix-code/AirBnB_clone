cc41163183b3c89b77454eccee7843cf07370718
hfog2066 <hfog2000@yahoo.com>
2020-11-04 00:02:21 -0500

    file_storage.py modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,8 +25,8 @@ class FileStorage:
 
     def new(self, obj):
         """Sets the obj in a key <obj class name>.id"""
-        key = type(obj).__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        k = type(obj).__name__ + '.' + obj.id
+        self.__objects[k] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
