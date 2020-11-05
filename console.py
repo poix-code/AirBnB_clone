41f5958d4531d0f79daa201abf69e8b17e8bc97f
poix-code <1996@holbertonschool.com>
2020-11-05 00:44:17 +0000

    Added the right imports to the console

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import models
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.state import State
+from models.review import Review
 classes = ['BaseModel', 'User', 'Place', 'State', 'City', 'Amenity', 'Review']
 
 
